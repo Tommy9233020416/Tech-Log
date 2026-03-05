231b6e2adf17f10b8bafabcc152fcd59c4792806
TOMMY_Wu <144784383+Tommy9233020416@users.noreply.github.com>
2026-03-05 19:40:34 +0800

    Delete build directory

--- build/my_drone_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/my_drone_description
-# Build directory: /root/catkin_ws/build/my_drone_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
